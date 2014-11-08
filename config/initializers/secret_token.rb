bd76094de3fddb7155378562cab769c2bc6ac927
Kumari Sweta <ksweta1007@gmail.com>
2014-11-08 00:18:02 -0800

    Initialized the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Monopoly::Application.config.secret_key_base = '40ed7cbb6644abf434dedc6bf0f4719590811294051f7f59d18a4ae16a286acddbcd83da93fb894a186fc79b93631554f19b1febb2003647eca24c5f360ca28e'
