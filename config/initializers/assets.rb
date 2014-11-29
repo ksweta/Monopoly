c09d92ff3a520c79e0a823c355792e666bc3ce71
Jacob Gronert <jake@Jakey-Debian>
2014-11-29 00:34:23 -0800

    This commit incorporates the new page specific css. The game board file has also been updated to reflect more closely how the final game will look.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,11 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+Rails.application.config.assets.precompile += %w( sign_in.css )
+Rails.application.config.assets.precompile += %w( welcome.css )
+Rails.application.config.assets.precompile += %w( host_game.css )
+Rails.application.config.assets.precompile += %w( game.css )
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
