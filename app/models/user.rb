32e925e335834d1d44910ca0f8bfbadd0f981b35
vinh <stevenlu7@yahoo.com>
2014-12-10 13:46:57 -0800

    user online list still not working very well

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :timeoutable
 
    has_many :players, inverse_of: :user
    has_many :games, through: :players
